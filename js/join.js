efec3e8ac60f4d5c657ff23fd2771369bb3e9da8
kelvinkyalo <kelvinchax@gmail.com>
2020-03-04 11:40:03 +0300

    add toogle effect

--- js/join.js
+++ js/join.js
@@ -1,19 +1,18 @@
-$(function() {
-    $("#choose").click(function() {
-        $("form").fadeoogle(1000);
+$(document).ready(function() {
+    $(".clickable").click(function() {
+        $("#hidden").toggle();
+        $("#showing").toggle();
     });
 });
-
-
-// $(document).ready(function() {
-//     $(".clickable2").click(function() {
-//         $("#hidden2").toggle();
-//         $("#showing2").toggle();
-//     });
-// });
-// $(document).ready(function() {
-//     $(".clickable3").click(function() {
-//         $("#hidden3").toggle();
-//         $("#showing3").toggle();
-//     });
-// });
+$(document).ready(function() {
+    $(".clickable2").click(function() {
+        $("#hidden2").toggle();
+        $("#showing2").toggle();
+    });
+});
+$(document).ready(function() {
+    $(".clickable3").click(function() {
+        $("#hidden3").toggle();
+        $("#showing3").toggle();
+    });
+});
