6cd206f3c48459a4838a0e89d5b65821bf24b2b6
kelvinkyalo <kelvinchax@gmail.com>
2020-03-04 11:37:17 +0300

    add inpit forms

---
+++ js/join.js
@@ -0,0 +1,19 @@
+$(function() {
+    $("#choose").click(function() {
+        $("form").fadeoogle(1000);
+    });
+});
+
+
+// $(document).ready(function() {
+//     $(".clickable2").click(function() {
+//         $("#hidden2").toggle();
+//         $("#showing2").toggle();
+//     });
+// });
+// $(document).ready(function() {
+//     $(".clickable3").click(function() {
+//         $("#hidden3").toggle();
+//         $("#showing3").toggle();
+//     });
+// });
