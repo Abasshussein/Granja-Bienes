1ffee0e19069614f9738ee9f961870aa2b99b32b
kevin <kevinothira@gmail.com>
2020-03-05 15:45:05 +0300

    read more options added

--- js/script.js
+++ js/script.js
@@ -28,3 +28,20 @@ function myFunction() {
     moreText.style.display = "inline";
   }
 }
+
+function myFunction1() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  }
+   else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
